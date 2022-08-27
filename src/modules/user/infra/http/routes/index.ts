7a6bf3d81ba323a54d14ac8faf432303567957eb
Andre Rocha <snitram@pop-os.localdomain>
2022-08-27 00:18:30 -0300

    feat: add integration wih aws s3

--- src/modules/user/infra/http/routes/index.ts
+++ src/modules/user/infra/http/routes/index.ts
@@ -2,13 +2,17 @@ import { Router } from "express";
 import { AuthenticateUserController } from "../controllers/AuthenticateUserController";
 import { CreateUserController } from "../controllers/CreateUserController";
 import { DeleteUserController } from "../controllers/DeleteUserController";
+import { UploadPhotoController } from "../controllers/UploadPhotoController";
 
 export const userRoutes = Router();
 
 const createUserController = new CreateUserController();
 const deleteUserController = new DeleteUserController();
 const authenticateUserController = new AuthenticateUserController();
+const uploadPhotoController = new UploadPhotoController();
 
 userRoutes.post("/", createUserController.handle);
 userRoutes.post("/login", authenticateUserController.handle);
 userRoutes.delete("/", deleteUserController.handle)
+
+userRoutes.post("/upload", uploadPhotoController.handle);
